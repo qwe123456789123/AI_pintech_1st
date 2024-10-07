d55e8d1a23db6b442b118f5cc17c1649fad4c45c
qwe123456789123 <p1199019@naver.com>
2024-10-07 16:53:54 +0900

    day01 수업 소스

---
+++ 4. 자바기초/source/day01/src/exam01/Ex07.java
@@ -0,0 +1,9 @@
+package exam01;
+
+public class Ex07 {
+    public static void main(String[] args) {
+        long num1 = 1000000000L; // int -> long
+        long num3 = 10_000_000_000L;
+        byte num2 = 100; // int 100 -> byte 변환
+    }
+}
