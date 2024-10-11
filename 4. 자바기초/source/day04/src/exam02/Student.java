7efffb2adbdc660b0ef05a9184afaa5423e73194
qwe123456789123 <p1199019@naver.com>
2024-10-11 18:25:00 +0900

    수업 소스

---
+++ 4. 자바기초/source/day04/src/exam02/Student.java
@@ -0,0 +1,34 @@
+package exam02;
+
+public class Student {
+
+    int id; // 학번
+    String name; // 학생명
+    String major;; // 전공과목
+
+    /**
+     * 따로 정의 하지 않으면 기본 생성되는 생성자
+     * (객체를 생성하는 기능)
+     * default
+     */
+
+    // 기본 생성자, 따로 정의  X -> 컴파일러가 추가, 디폴트 생성자
+    public Student() {
+        // 객체 생성 이후 실행
+        // 객체의 변수의 초기화
+        id = 1000;
+        name = "이이름";
+        major = "영어";
+    }
+
+    public Student(int _id, String _name, String _major){
+        id  = _id;
+        name = _name;
+        major = _major;
+    }
+
+
+    void showInfo() {
+        System.out.printf("id=%d, name=%s, major=%s%n",id,name,major);
+    }
+}
