6ba2f26f7fea7106aa41a0d5b1ef14b97addf71a
qwe123456789123 <p1199019@naver.com>
2024-10-28 17:16:04 +0900

    .

---
+++ 4. 자바기초/source/day13/src/exam01/Ex07.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class Ex07 {
+    private String title;
+
+    public void setTitle(String title){
+        title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+}
